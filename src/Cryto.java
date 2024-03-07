44595dc214e2399d1c438b1dc790ce3c495ec0d6
AnsaDavide <150423454+AnsaDavide@users.noreply.github.com>
2024-03-07 19:21:25 +0100

    modifiche

--- src/Cryto.java
+++ src/Cryto.java
@@ -15,13 +15,10 @@ public class Cryto {
 
         for (int i = 0; i < data.length(); i++) {
             char d = data.charAt(i);
-
-            for (int j = 0; j < key.length(); j++) {
-                char k = key.charAt(j);
+                char k = key.charAt(i%key.length());
 
                 char xorChar = (char) (d ^ k);
                 acc = acc + xorChar;
-            }
         }
 
         return acc;
